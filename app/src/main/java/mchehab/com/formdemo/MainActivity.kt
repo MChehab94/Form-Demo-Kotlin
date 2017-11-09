0bac2c1726181f8541043af32d0303f70fdb5367
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-09 23:24:19 +0200

    Initial commit

---
+++ app/src/main/java/mchehab/com/formdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package mchehab.com.formdemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+    }
+}
