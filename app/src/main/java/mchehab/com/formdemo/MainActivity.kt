7eaeeaaa0904f919cb65c6211adc37926d76f8ea
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-10 15:54:28 +0200

    Added broadcast, register/unregister handling, and checkbox toggle

--- app/src/main/java/mchehab/com/formdemo/MainActivity.kt
+++ app/src/main/java/mchehab/com/formdemo/MainActivity.kt
@@ -1,7 +1,13 @@
 package mchehab.com.formdemo
 
+import android.app.AlertDialog
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.CheckedTextView
@@ -52,11 +58,42 @@ class MainActivity : AppCompatActivity() {
         findViewById<EditText>(R.id.editTextDelivery)
     }
 
+    private var isJSONPosting = false
+    private val broadcastReceiverPost = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val bundle = intent.extras
+            if (bundle != null) {
+                val result = bundle.getString("result")
+                AlertDialog.Builder(this@MainActivity)
+                        .setTitle("Result")
+                        .setMessage(result)
+                        .setPositiveButton("Ok", null)
+                        .create()
+                        .show()
+                isJSONPosting = false
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiverPost, IntentFilter(BroadcastConstants.JSON_POST))
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+
+        checkboxExtraCheese.setOnClickListener { e -> checkboxExtraCheese.toggle() }
+        checkboxBacon.setOnClickListener { e -> checkboxBacon.toggle() }
+        checkboxMushroom.setOnClickListener { e -> checkboxMushroom.toggle() }
+        checkboxOnion.setOnClickListener { e -> checkboxOnion.toggle() }
 
         if (savedInstanceState != null) {
             checkboxBacon.isChecked = savedInstanceState.getBoolean("checkboxBacon")
