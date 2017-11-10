86c4cbb4f06a1964bc682467cc60717386d0871c
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-10 15:54:00 +0200

    Added broadcast constants

---
+++ app/src/main/java/mchehab/com/formdemo/BroadcastConstants.kt
@@ -0,0 +1,10 @@
+package mchehab.com.formdemo
+
+/**
+ * Created by muhammadchehab on 11/10/17.
+ */
+object BroadcastConstants {
+    val JSON = "json"
+    val IMAGE = "image"
+    val JSON_POST = "json post"
+}
